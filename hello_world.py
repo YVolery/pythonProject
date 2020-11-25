2561d00fe8c5a25446450a05c9b4d1113814e023
YVolery <71435317+YVolery@users.noreply.github.com>
2020-11-25 18:48:49 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5+1
+6
+>>> print("Hello Python world")
+Hello Python world
+>>> ub
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    ub
+NameError: name 'ub' is not defined
+>>> print("H")
+H
+>>> message="Hello Python world"
+>>> print(message)
+Hello Python world
+>>> message="Hello Python Crash Course world"
+
+>>>print(message)
+1+1
+
+1+1
+
+>>> 1+1
